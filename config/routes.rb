3d75690240e71d7af1f611394f7dc265e72abab0
Gabriele Baldocci <gabrielebaldocci@gabrielebaldocci.com>
2017-09-27 13:38:25 +0100

    Added nav bar and added default header content

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'about',   to: 'pages#about'
+  get 'about',   to: 'pages#about', as: 'about_me'
   get 'contact', to: 'pages#contact'
 
   resources :blogs do

939491e147b09911778c44dcc4a5d2f57d9a651c
Gabriele Baldocci <gabrielebaldocci@gabrielebaldocci.com>
2017-09-27 23:24:49 +0100

    Implemented view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def login_helper
+    unless current_user.is_a?(OpenStruct) 
+      link_to "Logout", destroy_user_session_path, method: :delete 
+    else 
+      (link_to "Sign Up", new_user_registration_path) + 
+      ("<br>".html_safe) +
+      (link_to "Login", new_user_session_path) 
+    end 
+  end
 end

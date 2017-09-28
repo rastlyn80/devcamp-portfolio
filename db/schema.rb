946032f84cc6e0a0f06ae22feb48e225706686b1
Gabriele Baldocci <gabrielebaldocci@gabrielebaldocci.com>
2017-09-28 19:38:46 +0100

    Integrated helper methods from ActionView

--- db/schema.rb
+++ db/schema.rb
@@ -71,12 +71,6 @@ ActiveRecord::Schema.define(version: 20170922110530) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "topis", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

ef133174d26c9fd7f8b131eecdc4fcf08ad157a8
Gabriele Baldocci <gabrielebaldocci@gabrielebaldocci.com>
2017-10-02 23:49:45 +0100

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,6 +6,10 @@ class Portfolio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
+
+
   def self.angular
     where(subtitle: 'Angular')
   end
@@ -19,7 +23,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: "600", width: "200")
+    self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
     self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")    
   end
 end

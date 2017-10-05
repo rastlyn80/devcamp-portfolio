ba238a49db0391b59c5a9f08d1a904b7957ec4b4
Gabriele Baldocci <gabrielebaldocci@gabrielebaldocci.com>
2017-10-06 00:33:54 +0100

    Added alerts to blogs and portfolio pages

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -24,7 +24,7 @@ class BlogsController < ApplicationController
       @page_title = @blog.title
       @seo_keywords = @blog.body
     else
-      redirect_to blogs_path, notice: "You are not authorized to access this page."
+      redirect_to blogs_path, notice: 'You are not authorized to access this page.' 
     end
   end
 
